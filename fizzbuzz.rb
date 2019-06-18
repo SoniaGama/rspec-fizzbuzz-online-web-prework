efade6d9485fb8457df84b2ca06aff6c54047e35
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 04:53:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuz(int)
+  if dog == "hungry"
+
+  elsif dog == "thirsty"
+
+  elsif dog == "playful"
+
+  elsif dog == "cuddly"
+
+  else
+
+  end
+end
