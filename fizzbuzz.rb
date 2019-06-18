bfee32c0929412144cc99b6c03d3fdc5cdf316f6
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 04:55:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuz(int)
-  if dog == "hungry"
-
-  elsif dog == "thirsty"
+  if int % 3 == 0 
+    "Fizz"
+  elsif int % 5 == 0 
+    "Buzz"
 
   elsif dog == "playful"
 
